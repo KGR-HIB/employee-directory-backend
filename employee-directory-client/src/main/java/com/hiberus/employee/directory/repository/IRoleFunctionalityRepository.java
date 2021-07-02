ba8476d6799b2fb77ea87579f06e543a7dfc5c6b
smxACL <acachiguango@ec.krugercorp.com>
2021-07-02 12:37:19 -0500

    KDE-40 Implementado consulta funcionalidades

--- employee-directory-client/src/main/java/com/hiberus/employee/directory/repository/IRoleFunctionalityRepository.java
+++ employee-directory-client/src/main/java/com/hiberus/employee/directory/repository/IRoleFunctionalityRepository.java
@@ -1,7 +1,9 @@
 package com.hiberus.employee.directory.repository;
 
-import com.hiberus.employee.directory.entity.RoleEntity;
+import java.util.List;
+import com.hiberus.employee.directory.entity.RoleFunctionalityEntity;
 import com.hiberus.employee.directory.repository.common.IQueryDslBaseRepository;
+import com.hiberus.employee.directory.vo.RoleFunctionality;
 
 /**
  * IRoleFunctionalityRepository.
@@ -9,6 +11,15 @@ import com.hiberus.employee.directory.repository.common.IQueryDslBaseRepository;
  * @author acachiguango on 02/07/2021
  * @version 1.0
  */
-public interface IRoleFunctionalityRepository extends IQueryDslBaseRepository<RoleEntity> {
+public interface IRoleFunctionalityRepository extends IQueryDslBaseRepository<RoleFunctionalityEntity> {
+
+    /**
+     * Obtiene funcionalidades por roleId.
+     * 
+     * @author acachiguango on 02/07/2021
+     * @param roleId id rol
+     * @return List RoleFunctionality
+     */
+    List<RoleFunctionality> findByRoleId(Integer roleId);
 
 }
